b0dcb05007bda233464545380cf6a231f22a22e3
yyylx <2969115050@qq.com>
2020-03-07 15:01:23 +0800

    first.py
    方程求解

---
+++ first.py
@@ -0,0 +1,18 @@
+import math
+a = float(input("请输入a的值："))
+b = float(input("请输入b的值："))
+c = float(input("请输入c的值："))
+if a != 0:
+    delta = b**2-4*a*c
+    if delta < 0:
+        print("无解")
+    elif delta == 0:
+        s = -b/(2*a)
+        print("唯一解x=",s)
+    else :
+        root = math.sqrt(delta)
+        x1 = (-b+root)/(2*a)
+        x2 = (-b-root)/(2*a)
+        print("有两个解：")
+        print("x1=",x1)
+        print("x2=",x2)
